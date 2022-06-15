0c80f8ce8a44c6efa8c65cf5a9dc310ca635aa4e
Atthiyyath <56459274+Atthiyya22194@users.noreply.github.com>
2022-06-15 09:59:08 +0700

    Adding speed up power up

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] float speed;
-
+    //[SerializeField] float speed;
+    public Vector2 speed;
+    public Vector2 resetPosition;
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        BallMovement();
+        rb.velocity = speed;
+        //BallMovement();
     }
 
-    private void BallMovement()
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime);
+    }
+
+    /*private void BallMovement()
     {
         float x = Random.Range(0, 2) == 0 ? -1 : 1;
         float y = Random.Range(0, 2) == 0 ? -1 : 1;
         rb.velocity = new Vector2(speed * x, speed * y);
+<<<<<<< Updated upstream
+=======
+    }*/
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
+    }
+
+    public void ActivatePUSpeedUp(float magnitude)
+    {
+        rb.velocity *= magnitude;
     }
 }
